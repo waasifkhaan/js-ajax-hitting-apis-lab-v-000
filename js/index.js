18ffa619d93d5672d1af744c0a176c27162eace2
Wasif Khan <waasif.khaan@gmail.com>
2018-11-14 16:41:53 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,54 +1,102 @@
-// your code here
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ @waasifkhaan Sign out
+19
+0 473 learn-co-students/js-ajax-hitting-apis-lab-v-000
+ Code  Issues 0  Pull requests 457  Projects 0  Insights
+js-ajax-hitting-apis-lab-v-000/js/index.js
+ce4a926  on Sep 4
+@maxwellbenton maxwellbenton updated solution, rebase from master
+     
+73 lines (71 sloc)  2.25 KB
 const rootURL = 'https://api.github.com';
+
 function getRepositories() {
-  name = document.getElementById('username').value
-  const uri = rootURL + '/users/' + name + '/respos'
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', displayRepositories)
-  req.open('GET', uri);
-  req.send();
-  return false
+  const name = document.getElementById('username').value;
+  const uri = rootURL + '/users/' + name + '/repos';
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayRepositories);
+  xhr.open('GET', uri);
+  xhr.send();
+  return false;
 }
-
-function displayRepositories(){
-  var repos = JSON.parse(this.responseText);
-    console.log(repos);
-    const repoList = `<ul>${repos
-      .map(
-        r =>
-          '<li>' +
-          r.name +
-          ' - <a href="#" data-repo="' +
-          r.name +
-          '" onclick="getCommits(this)">Get Commits</a></li>'
-      )
-      .join('')}</ul>`;
-    document.getElementById('repositories').innerHTML = repoList;
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const repoList =
+    '<ul>' +
+    repos
+      .map(repo => {
+        const dataUsername = 'data-username="' + repo.owner.login + '"';
+        const dataRepoName = 'data-repository="' + repo.name + '"';
+        return `
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`;
+      })
+      .join('') +
+    '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
 }
-
 function getCommits(el) {
-  const name = el.dataset.repo;
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', displayCommits);
-  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
-  req.send();
+  const repoName = el.dataset.repository;
+  const uri =
+    rootURL + '/repos/' + el.dataset.username + '/' + repoName + '/commits';
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayCommits);
+  xhr.open('GET', uri);
+  xhr.send();
 }
-
-def displayCommits() {
+function displayCommits() {
   const commits = JSON.parse(this.responseText);
   const commitsList = `<ul>${commits
     .map(
       commit =>
-        '<li><strong>' +
-        commit.author.name +
-        '</strong> - ' +
-        '<li><strong>' +
-        commit.author.fullname +
-        '</strong> - ' +
+        '<li><h3>' +
+        commit.commit.author.name +
+        ' (' +
+        commit.author.login +
+        ')</h3>' +
         commit.commit.message +
         '</li>'
     )
     .join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList;
-
 }
+function getBranches(el) {
+  const repoName = el.dataset.repository;
+  const uri =
+    rootURL + '/repos/' + el.dataset.username + '/' + repoName + '/branches';
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', displayBranches);
+  xhr.open('GET', uri);
+  xhr.send();
+}
+function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches
+    .map(branch => '<li>' + branch.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
+}
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Press h to open a hovercard with more details.
