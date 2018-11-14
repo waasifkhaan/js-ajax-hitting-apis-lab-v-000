6ee5c0206f26479ceccae3dfd90b756f34ee6fa4
Wasif Khan <waasif.khaan@gmail.com>
2018-11-14 16:15:41 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -10,6 +10,14 @@ function displayRepositories(){
 
 }
 
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
+
 def displaycommits() {
   const commits = JSON.parse(this.responseText);
   const commitsList = `<ul>${commits
