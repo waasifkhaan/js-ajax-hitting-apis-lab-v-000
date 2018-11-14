95f5734bf67357ba84bf028f8b444896af7d4e87
Wasif Khan <waasif.khaan@gmail.com>
2018-11-14 16:14:59 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // your code here
 function getRepositories() {
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories)
+  req.addEventListener('load', displayRepositories)
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
