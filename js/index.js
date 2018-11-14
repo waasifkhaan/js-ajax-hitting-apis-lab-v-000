b4fac219baaa70d07028dd8526ef42b4d5881d4c
Wasif Khan <waasif.khaan@gmail.com>
2018-11-14 16:12:11 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -9,3 +9,19 @@ function getRepositories() {
 function showRepositories(){
   
 }
+
+def displaycommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
+  
+}
